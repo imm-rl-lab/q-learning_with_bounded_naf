648bad7570bd3ff4a30f4f566640a3b47418af4f
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-05-24 15:28:24 +0500

    Update linear_transformations.py

--- models/linear_transformations.py
+++ models/linear_transformations.py
@@ -1,3 +1,10 @@
+import torch
+
+
 def transform_interval(value, value_min, value_max):
     'linear transformation making from the interval [-1, 1] the interval [value_min, value_max]'
+    if type(value) is torch.Tensor:
+        value_min = torch.FloatTensor(value_min)
+        value_max = torch.FloatTensor(value_max)
+
     return value * (value_max - value_min) / 2 + (value_max + value_min) / 2
